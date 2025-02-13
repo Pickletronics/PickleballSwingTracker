6647d694c7840c3160befa3a38ca4ad2ffca2567
Ryan Lewis <ryanlewis0117@gmail.com>
2025-02-12 20:11:43 -0500

    Magnetometer sampling

--- src/FSM.c
+++ src/FSM.c
@@ -44,6 +44,7 @@ static void PLAY_SESSION_Enter(int8_t button_input, FSM_state_t* state_handler) 
         if (state_handler->Play_Session_Handle == NULL) {
             // create session task
             state_handler->play_session_active = true;
+            LED_notify(BATTERY_LEVEL);
             xTaskCreatePinnedToCore(Play_Session_task, "Session_task", 4096, NULL, 1, &state_handler->Play_Session_Handle, 0);
             ESP_LOGI(FSM_TAG,"Play session started");
         }
